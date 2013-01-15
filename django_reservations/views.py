e06cfc80b058cb2f2088245861a8784163968367
Fei Tan <fei@argonemyth.com>
2013-01-15 11:14:48 +0400

    Using the json comes with python instead of simplejson.

--- django_reservations/views.py
+++ django_reservations/views.py
@@ -1,4 +1,5 @@
-import datetime, simplejson
+import datetime
+import json as simplejson
 
 from django.shortcuts import render_to_response, get_object_or_404
 from django.http import HttpResponseRedirect, Http404, HttpResponse
